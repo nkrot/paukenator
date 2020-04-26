051caf6a08a88aabe098e131454bcc7f5a5a478c
Nikolai Krot <talpus@gmail.com>
2020-04-26 22:54:20 +0200

    main script

--- src/paukenator/cli/main.py
+++ src/paukenator/cli/main.py
@@ -1,7 +1,28 @@
 import sys
+import argparse
+
+from ..text import Text
+from ..lesson import Lesson
+
+def parse_cmd_arguments():
+    parser = argparse.ArgumentParser(
+        description="TODO",
+        epilog="Have fun"
+    )
+    parser.add_argument('files', nargs='+',
+                        help='input file(s)')
+
+    args = parser.parse_args()
+    return args
 
 def main():
-    print("Hello dear")
+    args = parse_cmd_arguments()
+
+    for infile in args.files:
+        text = Text.load(infile, lang='deu')
+        lesson = Lesson(text)
+        lesson.run()
+
     return 0
 
 if __name__ == '__main__':
