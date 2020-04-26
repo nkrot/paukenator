b94fe1dbd8b10ba248d40b178de02bf8565c33b1
Nikolai Krot <talpus@gmail.com>
2020-04-26 18:52:08 +0200

    add simple setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools
+
+setup(
+    name='Paukenator',
+    description="A tool to help you learn a text by heart",
+    author="Nikolai Krot",
+    author_email="talpus@gmail.com",
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'paukenator = paukenator.__main__:main'
+        ]
+    }
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
